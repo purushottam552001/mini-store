c35a1e836c4cdc6ab0ae4e99e33ea68b9694c91a
purushottam552001 <prafulbharade557@gmail.com>
2026-09-18 12:39:59 +0530

    Fix ProductCard import case

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 
-import ProductCard from '../Components/ProductCard';
+import Productcard from '../Components/Productcard';
 import Loader from '../Components/Loader';
 
 function Products() {
